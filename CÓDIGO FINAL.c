f130f9667fbad41e6190f540aa8982de82d5b897
pedrolucasms2 <73369350+pedrolucasms2@users.noreply.github.com>
2024-04-16 22:10:27 -0300

    CÓDIGO FINAL V1.0.c

--- CÓDIGO FINAL.c
+++ CÓDIGO FINAL.c
@@ -3,21 +3,19 @@
 // Definições de pinos
 const int triggerPin = 13;
 const int echoPin = 12;
-const int buzzerPin = 14; // Defina o pino do buzzer
+const int buzzerPin = 14; 
 
 void setup() {
   Serial.begin(9600);
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(buzzerPin, OUTPUT); // Definir o pino do buzzer como saída
+  pinMode(buzzerPin, OUTPUT); 
 }
 
 void loop() {
-  // Limpar o pino trigger
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
 
-  // Enviar um pulso no pino trigger
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
@@ -34,56 +32,56 @@ void loop() {
   Serial.println(" cm");
 
   // Acionar o buzzer com base na distância
-  if (distance_cm <= 10) /* se a distância for menor ou igual a 10 cm, emita um bip contínuo */
+  if (distance_cm <= 10) /* se a distância for menor ou igual a 10 cm, será emitido um bip contínuo */
   {
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); // 
   }
   
-  else if (distance_cm <= 20) /* se a distância for menor ou igual a 20 cm, aumente a intensidade do alarme e mostre um aviso no LCD*/
+  else if (distance_cm <= 20) /* se a distância for menor ou igual a 20 cm, aumente a intensidade do alarme*/
   {
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(25);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); // 
     delay(25);
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(25);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
     delay(25);
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(25);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
   }
 
   else if (distance_cm <= 30) /* se a distância for menor ou igual a 30 cm, aumente a intensidade do alarme*/
   {
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(50);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
     delay(50);
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(50);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
     delay(50);
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(50);
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
   }
-  else if (distance_cm > 30 && distance_cm <= 40) /* se a distância for maior que 30 cm e menor ou igual a 40, diminua ligeiramente a intensidade do alarme*/
+  else if (distance_cm > 30 && distance_cm <= 40) /* se a distância for maior que 30 cm e menor ou igual a 40, diminui ligeiramente a intensidade do alarme*/
   {
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(50);
     noTone(buzzerPin);
-    delay(100); // Para o tom
+    delay(100); 
   }
-  else if (distance_cm > 40 && distance_cm <= 60) /* se a distância for maior que 40 cm e menor ou igual a 60, diminua a intensidade do alarme*/
+  else if (distance_cm > 40 && distance_cm <= 60) /* se a distância for maior que 40 cm e menor ou igual a 60, diminui a intensidade do alarme*/
   {
-    tone(buzzerPin, 1000); // Emite um tom de 1000 Hz
+    tone(buzzerPin, 1000); 
     delay(50);
     noTone(buzzerPin);
-    delay(300); // Para o tom
+    delay(300); 
   }
   else /* se a distância for maior que 60 cm, parará os bips*/
   {
-    noTone(buzzerPin); // Para o tom
+    noTone(buzzerPin); 
   }
 }
